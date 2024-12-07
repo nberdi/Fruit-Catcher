4b7a3651cb531b4860bd2260391bdb12860fd592
Nauka <berdinauryzbek@gmail.com>
2024-12-06 23:57:33 -0500

    initialized imgs and displayed the bucket; also added move function to control the bucket

--- game.py
+++ game.py
@@ -5,10 +5,31 @@ class Game:
         self.screen = pygame.display.set_mode((screen_width, screen_height))
         self.clock = pygame.time.Clock()
         
+        # imgs
+        self.bucket_img = pygame.transform.scale(bucket_img, (50, 50))
+        self.bucket_x = 335  # center
+        self.bucket_speed = 0.5
+        
+        self.fruit_imgs = [pygame.transform.scale(img, (40, 40)) for img in fruit_list]
+        self.bomb_img = pygame.transform.scale(bomb_img, (40, 40))
+        self.heart_img = pygame.transform.scale(heart_img, (30, 30))
+        
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and self.bucket_x > 0:
+            self.bucket_x -= self.bucket_speed
+        if keys[pygame.K_RIGHT] and self.bucket_x < self.screen.get_width() - 50:
+            self.bucket_x += self.bucket_speed
+        
     def run(self):
         while True:
             
             self.screen.fill((172, 209, 175))
+            
+            self.move()
+            
+            # Draw bucket
+            self.screen.blit(self.bucket_img, (self.bucket_x, 450))
             
             pygame.display.update()
             
