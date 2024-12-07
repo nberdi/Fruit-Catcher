d98f04865cced31b4d60ff0f1c03d97ea7fc58fa
Nauka <berdinauryzbek@gmail.com>
2024-12-06 23:55:55 -0500

    loaded all the imgs

--- settings.py
+++ settings.py
@@ -4,4 +4,20 @@ pygame.init()
 screen_width = 700
 screen_height = 500
 
+# bucket img
+bucket_img = pygame.image.load("imgs/bucket.png")
 
+
+# fruit imgs
+apple = pygame.image.load("imgs/apple.png")
+banana = pygame.image.load("imgs/banana.png")
+watermelon = pygame.image.load("imgs/watermelon.png")
+strawberry = pygame.image.load("imgs/strawberry.png")
+# fruit list
+fruit_list = [apple, banana, strawberry, watermelon]
+
+# bomb img
+bomb_img = pygame.image.load("imgs/bomb.png")
+
+# heart img
+heart_img = pygame.image.load("imgs/heart.png")
